6c67e5d68e830df17bf7557280742fb501d546fc
Peter Saunderson <peteasa@gmail.com>
2017-03-31 15:43:39 +0100

    added build from scratch script
    Signed-off-by: Peter Saunderson <peteasa@gmail.com>

---
+++ tools-src/build-clang-from-scratch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+STARTDIR="$(pwd)"
+SRCDIR="$( cd "$( dirname "$0" )" && pwd )"
+TOPDIR="$SRCDIR/.."
+
+cd $SRCDIR
+svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+
+cd $TOPDIR
+mkdir build
+cd build/
+cmake -G "Unix Makefiles" ../tools-src/llvm -DCMAKE_INSTALL_PREFIX=$TOPDIR/local > cmake.log 2>&1
+make -j 2 > make.log 2>&1
+
+cd $STARTDIR
